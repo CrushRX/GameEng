6545773c0e60f8a8c2fb3aa1fbf83994ddc3daf6
CrushRX <63191402+CrushRX@users.noreply.github.com>
2022-07-26 23:08:08 +0300

    logout

--- resources/view/layouts/header.php
+++ resources/view/layouts/header.php
@@ -10,8 +10,30 @@
                 </ul>
             </div>
             <div class="userfield">
-                <?=(Validator::getUserCheck())?'logged':''?>
+                <?php if(Validator::getUserCheck()){?>
+                        <p>user</p>
+                    <div class="user-form">
+                        <ul>
+                            <li><a href="">profile</a></li>
+                            <li><a href="">settings</a></li>
+                            <li><a onclick="logout()">logout</a></li>
+                        </ul>
+                    </div>
+                <?php } else {?>
+                    <ul>
+                        <li><a href="/auth">Авторизация</a></li>
+                        <li><a href="/register">Регистрация</a></li>
+                    </ul>
+                <?php }?>
             </div>
         </div>
     </div>
 </header>
+
+<script>
+    function logout()
+    {
+        document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+        location="/";
+    }
+</script>
